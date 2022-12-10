7e0ecc3af408632881f8c08604832e69556ebf47
Harlan Ferguson <64878230+hjferguson@users.noreply.github.com>
2022-12-09 19:07:49 -0500

    Delete setcookie.php

--- assignment3/controller/setcookie.php
+++
@@ -1,4 +0,0 @@
-<?php
-setcookie("user", $_SESSION['user'], time() + (86400 * 5), "/"); //set cookie for 5 days
-echo "Session variable is: " . $_SESSION['user'] . "<br>";
-echo "cookie is: " . $_COOKIE['user'] . "<br>";
